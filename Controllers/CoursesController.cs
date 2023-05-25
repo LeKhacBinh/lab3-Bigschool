3e629031beb421dd64d62be560298aef7e6cba3a
dell <dell@DESKTOP-4TKNDEH>
2023-05-25 23:42:30 +0700

    Add project files.

---
+++ Controllers/CoursesController.cs
@@ -0,0 +1,30 @@
+﻿using lab3.Models;
+using lab3.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab3.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Courses
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
